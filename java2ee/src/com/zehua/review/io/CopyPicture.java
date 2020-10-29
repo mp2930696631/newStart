f1677a25df68b184d3122593ec373fd7bed6fb99
数星星等天明 <891393221@qq.com>
2020-10-29 20:51:45 +0800

    课程类型：java零基础课程 章节：复习 讲解人：连老师 对应视频中的时间： 任务标题：io

---
+++ java2ee/src/com/zehua/review/io/CopyPicture.java
@@ -0,0 +1,39 @@
+package com.zehua.review.io;
+
+import java.io.*;
+
+public class CopyPicture {
+
+    public static void main(String[] args) {
+        String path = System.getProperty("user.dir");
+        String pictureName = "picture.jpg";
+        String copyPictureName = "copyPicture.jpg";
+        String fullName = path + File.separator + pictureName;
+        String copyPicturefullName = path + File.separator + copyPictureName;
+
+        File file = new File(fullName);
+        File copyPictureFile = new File(copyPicturefullName);
+        if (!file.exists()) {
+            System.out.println("picture does not exist ,return");
+            return;
+        }
+
+        try (
+                FileInputStream fis = new FileInputStream(file);
+                FileOutputStream fos = new FileOutputStream(copyPictureFile);
+        ) {
+            byte[] buffer = new byte[1024];
+            int length = 0;
+            while ((length = fis.read(buffer)) != -1) {
+                fos.write(buffer, 0, length);
+            }
+
+            fos.flush();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
