f82f5b18049482e3032a1bd4c016da2b110cfe34
zehua <891393221@qq.com>
2020-11-14 15:12:32 +0800

    课程类型：java零基础后端 章节： 讲解人：连老师 对应视频中的时间： 任务标题：springmvc简单应用

--- springmvc-review/src/main/java/com/hz/practice/interceptor/MyInterceptor1.java
+++ springmvc-review/src/main/java/com/hz/practice/interceptor/MyInterceptor1.java
@@ -1,6 +1,7 @@
 package com.hz.practice.interceptor;
 
 import com.hz.practice.controller.InterceptorController;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -8,6 +9,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
+ * 第三个参数类型是HandlerMethod
+ *
  * @author: zehua
  * @date: 2020/11/14 14:02
  */
@@ -17,6 +20,8 @@ public class MyInterceptor1 implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         if (handler instanceof InterceptorController){
             System.out.println("ccccccccooooooo111111");
+        }else if (handler instanceof HandlerMethod){
+            System.out.println(((HandlerMethod)handler).getBeanType());
         }else {
             System.out.println(handler);
         }
