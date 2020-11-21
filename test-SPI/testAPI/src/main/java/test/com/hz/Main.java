c4f84ee28d1f686eb6065d56c9c5bf9c7fa833af
zehua <891393221@qq.com>
2020-11-21 09:00:12 +0800

    测试SPI

--- test-SPI/testAPI/src/main/java/test/com/hz/Main.java
+++ test-SPI/testAPI/src/main/java/test/com/hz/Main.java
@@ -7,6 +7,8 @@ import interf.com.hz.DriverManager;
  * 其实没什么神奇的，主要依赖于ServiceLoad这个类，
  * 获取其Iterator对象并进行遍历的时候就会去classpath*：/META-INF/services/目录下面去寻找相应的实现类
  *
+ * 注意：如果想在其他maven项目中引用本maven项目，那么，本项目的目录结构必须以<groupId>interf.com.hz</groupId>开头
+ *
  * @author zehua
  * @date 2020/11/21 8:41
  */
